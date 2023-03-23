588081f864286670f72281255750e7231ad345c8
Julien Le Mee <julemee@gmail.com>
2023-03-23 16:32:55 -0400

    create Navbar and Hero components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+import Hero from '@/components/Hero'
+import Navbar from '@/components/Navbar'
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,7 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-center text-red-800'>Welcome to Teslo</h1>
+      <Navbar />
+      <Hero />
     </>
   )
 }
