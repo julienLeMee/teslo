8e8d9772d7b0a05b8ce42ba60c9e9d11aca90c84
Julien Le Mee <julemee@gmail.com>
2023-03-24 00:04:00 -0400

    added Navbar and Hero components

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
+import { BsChevronDown } from 'react-icons/bs'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className="w-full h-screen bg-hero text-[#181A1F]">
+        <div className="flex flex-col justify-between h-full py-10">
+          <h1 className="text-4xl tracking-wider font-medium text-center pt-20">Model 3</h1>
+
+          <div className="flex flex-col">
+            <div className="w-full md:w-[40%] max-w-[90%] mx-auto flex items-center justify-center py-4">
+              <div className="flex flex-col md:flex-row items-center justify-between w-full">
+                <Link
+                  href='/'
+                  className="bg-black/70 w-full text-center m-2 my-2 md:my-0 rounded text-white text-sm h-full cursor-pointer">
+                  <span className='py-1 mt-2 flex items-center justify-center h-full my-auto'>
+                    Custom Order
+                  </span>
+                </Link>
+                <Link
+                  href='/'
+                  className="bg-white/80 w-full text-center m-2 my-2 md:my-0 rounded text-[#181A1F] text-sm h-full cursor-pointer">
+                  <span className='py-1 mt-2 flex items-center justify-center h-full my-auto'>
+                    Demo Drive
+                  </span>
+                </Link>
+              </div>
+            </div>
+
+            <BsChevronDown className='text-xl animate-pulse text-center w-full' style={{animationDuration: '2s'}} />
+
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
+import logo from '../public/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="w-full flex items-center justify-between py-4 px-8 fixed text-black/80 font-medium">
+      <Link href='/'>
+        <Image
+          src={logo}
+          width={80}
+          height='auto'
+          alt='logo'
+          className='cursor-pointer object-cover mx-4 my-2'
+          priority
+        />
+      </Link>
+
+      <div>
+        <ul className='flex tracking-wide text-sm'>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/'>Model S
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/about'>Model 3
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/contact'>Model X
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/contact'>Model Y
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/contact'>Cybertruck
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/contact'>Powerwall
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className='flex tracking-wide text-sm'>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/'>Shop
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/about'>Account
+            </Link>
+          </li>
+          <li className='h-full ml-2 my-auto'>
+            <Link
+              className='mr-4'
+              href='/contact'>Menu
+            </Link>
+            </li>
+        </ul>
+      </div>
+
+    </div>
+    </>
   )
 }
 
