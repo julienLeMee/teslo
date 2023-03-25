87223c1f9274bc8a6799691a2ed7669b7685cdc9
Julien Le Mee <julemee@gmail.com>
2023-03-25 13:40:50 -0400

    added Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,43 +9,43 @@ const Footer = () => {
           <ul className='tracking-wide text-sm hidden sm:flex'>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Tesla 2023
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Privacy & Legal
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Vehicle recalls
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Contact
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Careers
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>News
               </Link>
             </li>
             <li className='h-full my-auto cursor-pointer hover:bg-black/10 duration-300 pt-2 rounded'>
               <Link
-                className='p-4 text-xs'
+                className='p-2 text-xs'
                 href='/'>Locations
               </Link>
             </li>
