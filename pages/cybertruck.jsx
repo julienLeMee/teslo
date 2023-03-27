360bee7dec1cd55c5fb1af98f2886426b479e526
Julien Le Mee <julemee@gmail.com>
2023-03-27 13:41:01 -0400

    added detail page powerwall

--- pages/cybertruck.jsx
+++ pages/cybertruck.jsx
@@ -14,7 +14,7 @@ const cybertruck = () => {
           </div>
             <div className="w-full h-32 md:w-[50%] max-w-[90%] mx-auto flex justify-center text-white">
               <div className="flex flex-col w-full items-center justify-around">
-                <div className="w-[50%] flex flex-col md:flex-row items-center justify-center mx-auto">
+                <div className="w-[40%] flex flex-col md:flex-row items-center justify-center mx-auto">
                   <Link
                     href='/'
                     className="hover:bg-white/90 hover:text-black mr-2 w-full mx-auto text-center m-2 my-2 md:my-0 rounded text-white text-sm h-full cursor-pointer border-4 duration-300">
