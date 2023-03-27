20f9b440756db07485fa15123db0b28c63913b6d
Julien Le Mee <julemee@gmail.com>
2023-03-26 23:59:55 -0400

    added models folder

---
+++ pages/models/cybertruck.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const cybertruck = () => {
+  return (
+    <div>cybertruck</div>
+  )
+}
---
+++ pages/models/modelx.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const modelx = () => {
+  return (
+    <div>modelx</div>
+  )
+}
+
+export default modelx
